9727fecb5f3619f6dfd637f01ced80cab4153db3
Rustam Manakov <lon10@rambler.ru>
2014-12-17 17:59:18 +0300

    Slice

--- AwesomeSequence.rb
+++ AwesomeSequence.rb
@@ -7,31 +7,31 @@
 require 'pry'
 
 class AwesomeSequence
-	def initialize(count)
-		@current_member = 1
-		fill_sequence(count)
-	end
+  def initialize(count)
+    @current_member = 1
+    fill_sequence(count)
+  end
 
-	private
+  private
 
-	def fill_sequence(count)
-		count.times do
-			p(@current_member = str_chunk.join(''))
-		end
-	end
+  def fill_sequence(count)
+    count.times do
+      p(@current_member = str_chunk.join(''))
+    end
+  end
 
-	def str_chunk
-		string_to_array(@current_member).chunk{|elt| elt}.map do |key, ary|
-			[
-				ary.size, 
-				key
-			]
-		end
-	end
+  def str_chunk
+    string_to_array(@current_member).chunk{|elt| elt}.map do |key, ary|
+      [
+        ary.size, 
+        key
+      ]
+    end
+  end
 
-	def string_to_array(str)
-		str.to_s.split('')
-	end
+  def string_to_array(str)
+    str.to_s.split('')
+  end
 end
 
 AwesomeSequence.new(6)
